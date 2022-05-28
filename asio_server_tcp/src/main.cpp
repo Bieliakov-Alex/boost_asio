7389fe089b765ff2fd9660db56d6b209f5c4c84c
Alex Bieliakov <bieliakov.alex@gmail.com>
2022-05-28 17:34:37 +0300

    Add tcp server example
    Added synchronized tcp server example

---
+++ asio_server_tcp/src/main.cpp
@@ -0,0 +1,34 @@
+#include <boost/asio.hpp>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+std::string make_daytime_string() {
+  std::time_t now = time(0);
+  return std::ctime(&now);
+}
+
+int main() {
+  try {
+    boost::asio::io_context io_context;
+    boost::asio::ip::tcp::acceptor acceptor(
+        io_context,
+        boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 13));
+
+    for (;;) {
+      boost::asio::ip::tcp::socket socket(io_context);
+      acceptor.accept(socket);
+
+      std::cout << "Incomming connection." << std::endl;
+
+      std::string message = make_daytime_string();
+
+      boost::system::error_code ignored_error;
+      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+    }
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
